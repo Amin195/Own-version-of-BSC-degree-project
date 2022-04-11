e9d56aaa62ddc470161977dddfa283854b9ff6e0
Amin195 <motherfucker0936954A>
2022-04-11 23:55:28 +0200

    playing around with the DB manager but nothing works!

--- db-manager.js
+++ db-manager.js
@@ -1,11 +1,3 @@
 const  mysql = require('mysql')
 
 module.exports = mysql.createConnection({ host: 'localhost', user: 'root1', password: '0936954', database: 'snowbeez', port: 3306})
-const con = 
-
-con.connect((err) => {
-    if (err) console.log('bad luck - no connection' + err)
-    else {
-        console.log('db connected')
-    }
-})
