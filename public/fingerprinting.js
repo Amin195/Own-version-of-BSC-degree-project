c730751b0c7aa1339fedec1a2608c913a3fb6273
Amin195 <motherfucker0936954A>
2022-04-13 22:32:17 +0200

    Can now get canvas fingerprint. Maybe move the fingerprinting into its own JS

--- public/fingerprinting.js
+++ public/fingerprinting.js
@@ -1,18 +1,48 @@
-const fpPromise = import('https://openfpcdn.io/fingerprintjs/v3')
-    .then(FingerprintJS => FingerprintJS.load())
+//const fpPromise = import('https://openfpcdn.io/fingerprintjs/v3')
+//     .then(FingerprintJS => FingerprintJS.load())
 
-// Get the visitor identifier when you need it.
-fpPromise
-    .then(fp => fp.get())
-    .then(result => {
-        // This is the visitor identifier:
-        const visitorId = result.visitorId
-        document.querySelector('form').addEventListener('submit', async () => {
-            console.log('Visitor ID when submitting: ', visitorId)
-            await postFP('http://localhost:3000/fp', { fingerprintJS: visitorId })
-        });
-        console.log(visitorId)
-    })
+// // Get the visitor identifier when you need it.
+// fpPromise
+//     .then(fp => fp.get())
+//     .then(result => {
+//         //console.log(result.components)
+//         // This is the visitor identifier:
+//         const visitorId = result.visitorId
+
+//         // Trying ti goet only canvas but facing some problems https://github.com/fingerprintjs/fingerprintjs/blob/master/docs/extending.md 
+
+//         //  const { audio, colorDepth, colorGamut, contrast, cookiesEnabled, cpuClass, deviceMemory, domBlockers, fontPreferences, fonts, forcedColors, hardwareConcurrency, hdr,
+//         //     indexedDB, invertedColors, languages, localStorage, math, monochrome, openDatabase, osCpu, platform, plugins, reducedMotion, screenFrame, screenResolution, sessionStorage, 
+//         //     timezone, touchSupport, vendor, vendorFlavors, ...components } = result.components
+//         //console.log('RESULT AFTER EXCLUDING ALL COMPONENTS', result)
+//         // const visitorId = FingerprintJS.hashComponents(components)
+
+//         document.querySelector('form').addEventListener('submit', async () => {
+//             console.log('Visitor ID when submitting: ', visitorId)
+//             await postFP('http://localhost:3000/fp', { fingerprintJS: visitorId })
+//         });
+//         console.log(visitorId)
+//     })
+
+async function initFingerprintJS() {
+    const fp = await FingerprintJS.load()
+    const result = await fp.get()
+
+    // Exclude a couple components
+    const { audio, colorDepth, colorGamut, contrast, cookiesEnabled, cpuClass, deviceMemory, domBlockers, fontPreferences, fonts, forcedColors, hardwareConcurrency, hdr,
+             indexedDB, invertedColors, languages, localStorage, math, monochrome, openDatabase, osCpu, platform, plugins, reducedMotion, screenFrame, screenResolution, sessionStorage, 
+            timezone, touchSupport, vendor, vendorFlavors, ...components } = result.components
+
+    // Make a visitor identifier from your custom list of components
+    const visitorId = FingerprintJS.hashComponents(components)
+    console.log('visitorId:', visitorId)
+
+    document.querySelector('form').addEventListener('submit', async () => {
+        console.log('Visitor ID when submitting: ', visitorId)
+        await postFP('http://localhost:3000/fp', { fingerprintJS: visitorId })
+    });
+}
+
 async function postFP(url, body) {
     const data = {
         method: 'POST',
@@ -37,3 +67,4 @@ async function postFP(url, body) {
 }
 
 //TODO use information from here to understand how to get CANVAS only!
+//TODO Check this link to work FingerprintJS to get canvas https://github.com/fingerprintjs/fingerprintjs/issues/607
