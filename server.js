ef4526929505c90b5e445b69746abe143394e90b
Dillon Jonsson <dj222jm@student.lnu.se>
2022-04-14 12:51:05 +0200

    last commit before merge

--- server.js
+++ server.js
@@ -47,14 +47,10 @@ app.get('/graphical', (req, res) => {
     res.render('graphical.ejs')
 })
 
-app.post('/graphical', async (req, res) => {
-    console.log('User graphical: ',req.body.paswordNumbers)
-})
-
 // Adding a route to handle new graphical testing
 // app.post('/graphical', graphicalManager.checkPictures)
 app.post('/gp', async (req, res)=> {
-    console.log('User graphical: ',req.body.paswordNumbers)
+    console.log('User graphical2: ',req.body)
 })
 
 
